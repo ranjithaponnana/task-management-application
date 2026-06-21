e82562f365fff1b17bb183bd8c895a3916112320
ranjithaponnana <ranjithaponnana2007@gmail.com>
2026-06-21 12:53:19 +0530

    Create models.py

---
+++ backend/models.py
@@ -0,0 +1,21 @@
+class User:
+
+    def __init__(self, username, email, password):
+
+        self.username = username
+
+        self.email = email
+
+        self.password = password
+
+
+
+class Task:
+
+    def __init__(self, title, description, status):
+
+        self.title = title
+
+        self.description = description
+
+        self.status = status
