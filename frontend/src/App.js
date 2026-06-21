dd8831e9f447d660448510c8e72bf0ed9153355b
ranjithaponnana <ranjithaponnana2007@gmail.com>
2026-06-21 12:59:02 +0530

    Create App.js

---
+++ frontend/src/App.js
@@ -0,0 +1,27 @@
+import React from "react";
+
+
+function App(){
+
+return(
+
+<div>
+
+<h1>
+Ranjitha Task Manager
+</h1>
+
+
+<p>
+Manage your daily tasks easily
+</p>
+
+
+</div>
+
+)
+
+}
+
+
+export default App;
